6737776c69f1ebb106bbc5897420ccee81d6939e
Yu-Heng Lin <yuhenglin0204@gmail.com>
2022-07-04 00:13:09 -0700

    Add files via upload

---
+++ crud_django/post/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from .models import Post
+from django.contrib import messages
+# Create your views here.
+
+
+def home(request):
+    posts = Post.objects.all() 
+    return render(request, 'home.html', {'posts': posts})
+
+# add method
+def add(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        detail = request.POST['detail']
+        Post.objects.create(title=title, detail=detail)
+        messages.success(request, 'Post has been added')
+    return render(request, 'add.html')
+
+# update method
+def update(request, id):
+    if request.method == 'POST':
+        title = request.POST['title']
+        detail = request.POST['detail']
+        Post.objects.filter(id=id).update(title=title, detail=detail)
+        messages.success(request, 'Post has been updated')
+    post = Post.objects.get(id=id)
+    return render(request, 'update.html', {'post':post})
+    
+
+
+# delete method
+def delete(request, id):
+    Post.objects.filter(id=id).delete()
+    return redirect('/')
